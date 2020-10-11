4c38b5b18f7c40725ea347c2791f316fe5c1859b
Bob Hunt <bob.hunt.email@gmail.com>
2020-10-10 22:42:39 -0400

    Reworked the render on App.js to aid in flexbox styling. Created more classes in App.css to match the changes in App.js.

--- src/App.js
+++ src/App.js
@@ -42,41 +42,59 @@ class App extends Component {
 
           <div className="keypad">
             <div className="numberpad"></div>
-              <div className="row">
-                <ClearBtn handleClear={() => this.setState({calcDisplay: "0"})}>C</ClearBtn>
-                <div className="blank"></div>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>/</OperatorBtn>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>*</OperatorBtn>
+
+              <div className="upper-pad">
+                <div className="row">
+                  <ClearBtn handleClear={() => this.setState({calcDisplay: "0"})}>C</ClearBtn>
+                  <div className="blank">?</div>
+                  <OperatorBtn handleClick={this.addToCalcDisplay}>/</OperatorBtn>
+                  <OperatorBtn handleClick={this.addToCalcDisplay}>*</OperatorBtn>
+                </div>
+                <div className="row">
+                  <NumBtn handleClick={this.addToCalcDisplay}>7</NumBtn>
+                  <NumBtn handleClick={this.addToCalcDisplay}>8</NumBtn>
+                  <NumBtn handleClick={this.addToCalcDisplay}>9</NumBtn>
+                  <OperatorBtn handleClick={this.addToCalcDisplay}>-</OperatorBtn>
+                </div>
+                <div className="row">
+                  <NumBtn handleClick={this.addToCalcDisplay}>4</NumBtn>
+                  <NumBtn handleClick={this.addToCalcDisplay}>5</NumBtn>
+                  <NumBtn handleClick={this.addToCalcDisplay}>6</NumBtn>
+                  <OperatorBtn handleClick={this.addToCalcDisplay}>+</OperatorBtn>
+                </div>
               </div>
-              <div className="row">
-                <NumBtn handleClick={this.addToCalcDisplay}>7</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>8</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>9</NumBtn>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>-</OperatorBtn>
+
+              <div className="lower-pad">
+
+                <div className="lower-pad left">
+                  <div className="lower-left" id="top">
+                    <div className="row">
+                      <NumBtn handleClick={this.addToCalcDisplay}>1</NumBtn>
+                      <NumBtn handleClick={this.addToCalcDisplay}>2</NumBtn>
+                      <NumBtn handleClick={this.addToCalcDisplay}>3</NumBtn>
+                    </div>
+                  </div>
+                  <div className="lower-left" id="bottom">
+                    <NumBtn className="zero" handleClick={this.addToCalcDisplay}>0</NumBtn>
+                    <NumBtn handleClick={this.addToCalcDisplay}>.</NumBtn>
+                  </div>
+                </div>
+
+                <div className="lower-pad right">
+                  <div className="operatorBtn equal">
+                    <OperatorBtn handleClick={this.addToCalcDisplay}>=</OperatorBtn>
+                  </div>
+                </div>
+
               </div>
-              <div className="row">
-                <NumBtn handleClick={this.addToCalcDisplay}>4</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>5</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>6</NumBtn>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>+</OperatorBtn>
-              </div>
-              <div className="row">
-                <NumBtn handleClick={this.addToCalcDisplay}>1</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>2</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>3</NumBtn>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>=</OperatorBtn>
-              </div>
-              <div className="row">
-                <NumBtn className="zero-btn" handleClick={this.addToCalcDisplay}>0</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>.</NumBtn>
-              </div>
+
             </div>
-            <div className="operaterpad">
+            {/* <div className="operaterpad">
               <OperatorBtn handleClick={this.addToCalcDisplay}>*</OperatorBtn>
               <OperatorBtn handleClick={this.addToCalcDisplay}>-</OperatorBtn>
               <OperatorBtn handleClick={this.addToCalcDisplay}>+</OperatorBtn>
               <OperatorBtn handleClick={() => this.handleEqual()}>=</OperatorBtn>
-            </div>
+            </div> */}
           </div>
       </div>
     );
