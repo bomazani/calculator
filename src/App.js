6586b4dfa1359eb61de24561a537703aed875e9d
Bob Hunt <bob.hunt.email@gmail.com>
2020-10-12 13:58:58 -0400

    Resolved merge conflict with dev. Deleted old render code.

--- src/App.js
+++ src/App.js
@@ -60,48 +60,6 @@ class App extends Component {
             <CalcDisplay calcDisplay={this.state.calcDisplay} />
           </div>
 
-          {/* flexbox */}
-          {/* <div className="keypad">
-            <div className="numberpad">
-              <div className="row">
-                <ClearBtn handleClear={() => this.setState({calcDisplay: "0"})}>C</ClearBtn>
-                <div className="blank">?</div>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>/</OperatorBtn>
-              </div>
-              <div className="row">
-                <NumBtn handleClick={this.addToCalcDisplay}>7</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>8</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>9</NumBtn>
-              </div>
-              <div className="row">
-                <NumBtn handleClick={this.addToCalcDisplay}>4</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>5</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>6</NumBtn>
-              </div>
-              <div className="row">
-                <NumBtn handleClick={this.addToCalcDisplay}>1</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>2</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>3</NumBtn>
-              </div>
-              <div className="row">
-                <NumBtn className="zero" handleClick={this.addToCalcDisplay}>0</NumBtn>
-                <NumBtn handleClick={this.addToCalcDisplay}>.</NumBtn>
-              </div>
-            </div>
-
-            <div className="operatorpad">
-              <div className="mult-minus-add-btns">
-                <OperatorBtn handleClick={this.addToCalcDisplay}>*</OperatorBtn>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>-</OperatorBtn>
-                <OperatorBtn handleClick={this.addToCalcDisplay}>+</OperatorBtn>
-              </div>
-              <div className="equalbtn">
-                <EqualBtn handleClick={this.handleEqual}>=</EqualBtn>
-              </div>
-            </div>
-          </div> */}
-
-          {/* grid and flexbox */}
           <div className="keypad">
               {/* <div className="row-1"> */}
                 <ClearBtn id="btn-clear" handleClear={() => this.setState({calcDisplay: "0"})}>C</ClearBtn>
